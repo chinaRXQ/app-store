66e5163666c90a585541988515b0301f61326308
rxq <894058740@qq.com>
2018-04-25 18:01:31 +0800

    seller组件更新
    seller组件更新

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import { urlParse } from 'common/js/util';
   import header from 'components/header/header';
 
   const ERR_OK = 0;
@@ -27,14 +28,20 @@
     name: 'App',
     data () {
       return {
-        seller: {}
+        seller: {
+          id: (() => {
+            let queryParam = urlParse();
+            return queryParam.id;
+          })()
+        }
       };
     },
     created () {
-      this.$http.get('/api/seller').then((response) => {
+      this.$http.get('/api/seller?id=' + this.seller.id).then((response) => {
         response = response.body;
         if (response.errno === ERR_OK) {
-          this.seller = response.data;
+          this.seller = Object.assign({}, this.seller, response.data);
+          console.log(this.seller.id);
         }
       });
     },
